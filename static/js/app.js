62cffb8f2045ef689b6b5887b3d8a82fea291a88
kate <kate_spitzer@yahoo.com>
2021-03-14 12:29:31 -0700

    work on bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -11,7 +11,6 @@ d3.json(dataFile).then((importedData) => {
 	var demographics = importedData.metadata;
 	var samples = importedData.samples;
 
-	console.log(samples);
 
 	// locate the target for the dropdown values
 	var dropdownTarget = d3.select("#selDataset");
@@ -36,17 +35,12 @@ d3.json(dataFile).then((importedData) => {
 		console.log(idChosen);
   
 
-
 		function filterId(data) {
 			return data.id == idChosen;
 		}
   
 		// 2. Use filter() to pass the function as its argument
 		var currDemo = demographics.filter(filterId);
-	//	console.log(currDemo);
-
-		var currSample = samples.filter(filterId);
-		console.log(currSample);
 
 		var demoTarget = d3.select("#sample-metadata");
 		demoTarget.html("");
@@ -55,28 +49,69 @@ d3.json(dataFile).then((importedData) => {
 
 		// loop through the key/value pairs in the metadata
 		// and display them in the panel
-
 		var demoKeys = Object.keys(currDemo[0]);
-//			console.log(demoKeys);
 			demoKeys.forEach(key => {
 				var demoRow = demoTbl.append("tr");
 				demoRow.append("td").text((`${key}: ${currDemo[0][key]}`));
 		});
+
+		var currSample = samples.filter(filterId);
+		console.log(currSample);
+
+		var otuIds = currSample[0].otu_ids;
+		var otuLabels = currSample[0].otu_labels;
+		var sampleValues = currSample[0].sample_values;
+		// console.log(otuIds);
+		// console.log(otuLabels);
+		// console.log(sampleValues);
+
+		var otuObject = [];
+
+		for (var i = 0; i < otuIds.length; i++) {
+			var tempObject = {};
+
+			tempObject = { "otu_id": otuIds[i], "otu_label": otuLabels[i], "sample_value": sampleValues[i]};
+			otuObject.push(tempObject);
+		}
+		console.log(otuObject);
+
+		// Sort the data by Greek search results
+//		var sortedSampleValues = data.sort((a, b) => b.sample - a.greekSearchResults);
+
+// Slice the first 10 objects for plotting
+//slicedData = sortedByGreekSearch.slice(0, 10);
+
+// Reverse the array to accommodate Plotly's defaults
+//reversedData = slicedData.reverse();
+
+// Trace1 for the Greek Data
+// var trace1 = {
+//   x: reversedData.map(object => object.greekSearchResults),
+//   y: reversedData.map(object => object.greekName),
+//   text: reversedData.map(object => object.greekName),
+//   name: "Greek",
+//   type: "bar",
+//   orientation: "h"
+// };
+
+// // data
+// var data = [trace1];
+
+// // Apply the group bar mode to the layout
+// var layout = {
+//   title: "Greek gods search results",
+//   margin: {
+//     l: 100,
+//     r: 100,
+//     t: 100,
+//     b: 100
+//   }
+// };
+
+// // Render the plot to the div tag with id "plot"
+// Plotly.newPlot("plot", data, layout);
+
   
-//   //  Check to make sure your filtered your cities.
-//   console.log(filteredCities);
-  
-//   // 3. Use the map method with the arrow function to return all the filtered cities.
-//   var cities = filteredCities.map(city => city.City);
-  
-//   //  Check your filtered cities
-//   console.log(cities);
-  
-//   // 4. Use the map method with the arrow function to return all the filtered cities population.
-//   var population = filteredCities.map(city => city.population);
-  
-//   //  Check the populations of your filtered cities
-//   console.log(population);
 	}
   
 });
